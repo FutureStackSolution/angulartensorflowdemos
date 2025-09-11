a74a9ec0be62be6c8b29dddcc079f430bd3beeb1
Jalpesh Vadgama <jalpesh.vadgama@gmail.com>
2025-09-11 20:05:24 +0530

    Added object detection changes in angular project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { PupilConcentrationTrackerComponent } from './pupil-concentration-tracker/pupil-concentration-tracker.component';
+import { ObjectDection } from './object-dection/object-dection';
 
 export const routes: Routes = [
   { path: 'eye-tracker', component: PupilConcentrationTrackerComponent },
+  { path: 'object-detection', component: ObjectDection },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
---
+++ src/app/object-dection/object-dection.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-object-dection',
+  imports: [],
+  templateUrl: './object-dection.html',
+  styleUrl: './object-dection.css'
+})
+export class ObjectDection {
+
+}
